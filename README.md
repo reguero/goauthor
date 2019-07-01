09474c22a3cccc49181336945c73a77c1fddc71e
Ignacio Reguero <Ignacio.Reguero@cern.ch>
2019-07-01 17:38:16 +0200

    first go to authorize using ldap

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # goauthor
+# Authorization tests in Go
---
+++ authorize.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+
+	"github.com/go-ldap/ldap"
+)
+
+func main() {
+	fmt.Printf("This is an authorization proof of concept\n")
+	if checkEgroup("ermis-lbaas-admins", "reguero") == true {
+		fmt.Printf("true\n")
+	} else {
+		fmt.Printf("false\n")
+	}
+	if checkEgroup("ermis-lbaas-admins", "toto") == true {
+		fmt.Printf("true\n")
+	} else {
+		fmt.Printf("false\n")
+	}
+	if checkEgroup("toto", "reguero") == true {
+		fmt.Printf("true\n")
+	} else {
+		fmt.Printf("false\n")
+	}
+	os.Exit(0)
+
+}
+
+func checkEgroup(egroup string, username string) bool {
+	l, err := ldap.Dial("tcp", "xldap.cern.ch:389")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer l.Close()
+
+	baseSuffix := "OU=Users,OU=Organic Units,DC=cern,DC=ch"
+	base := fmt.Sprintf("CN=%s,%s", username, baseSuffix)
+	filterSuffix := "OU=e-groups,OU=Workgroups,DC=cern,DC=ch)"
+	filter := fmt.Sprintf("(memberOf=CN=%s,%s", egroup, filterSuffix)
+	nestedFilter := fmt.Sprintf("(memberOf:1.2.840.113556.1.4.1941:=CN=%s,%s", egroup, filterSuffix)
+	exclDisabledPrefix := "(&(!(userAccountControl:1.2.840.113556.1.4.803:=2))(|"
+	attrs := []string{"dn", "cn"}
+	exclDisabled := true
+	if exclDisabled {
+		filter = exclDisabledPrefix + filter + "))"
+		nestedFilter = exclDisabledPrefix + nestedFilter + "))"
+	}
+	searchRequest := ldap.NewSearchRequest(
+		base, // The base dn to search
+		ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
+		filter, // The filter to apply
+		attrs,  // A list attributes to retrieve
+		nil,
+	)
+
+	re := regexp.MustCompile(".*No Such Object")
+	sr, err := l.Search(searchRequest)
+	if err != nil {
+		submatch := re.FindStringSubmatch(err.Error())
+		if submatch != nil {
+			fmt.Printf("Retrying\n")
+			searchRequest = ldap.NewSearchRequest(
+				base, // The base dn to search
+				ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
+				nestedFilter, // The filter to apply
+				attrs,        // A list attributes to retrieve
+				nil,
+			)
+			sr, err = l.Search(searchRequest)
+			if err != nil {
+				submatch := re.FindStringSubmatch(err.Error())
+				if submatch != nil {
+					fmt.Printf("Not Found\n")
+					return false
+				} else {
+					log.Fatal(err)
+				}
+			} else {
+				fmt.Printf("Found\n")
+				return true
+			}
+		} else {
+			log.Fatal(err)
+			return false
+		}
+	} else {
+		fmt.Printf("Found\n")
+		return true
+	}
+
+	for _, entry := range sr.Entries {
+		fmt.Printf("%s: %v\n", entry.DN, entry.GetAttributeValue("cn"))
+	}
+	return false
+}
