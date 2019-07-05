fce4286cc05f94e02d8a64b77df9fd8a0a2d0b67
Ignacio Reguero <Ignacio.Reguero@cern.ch>
2019-07-05 17:39:16 +0200

    still nogo

--- landbsoap/landbsoap.go
+++ landbsoap/landbsoap.go
@@ -14,16 +14,18 @@ func main() {
 		"https://cstest.cern.ch/sc/soap/soap.fcgi?v=6&WSDL",
 		//"https://network.cern.ch/sc/soap/soap.fcgi?v=5&WSDL",
 		soap.WithTimeout(time.Second*5),
+		soap.WithBasicAuth("xxxx", "yyy"),
 		soap.WithTLS(&tls.Config{InsecureSkipVerify: true}),
 	)
 	service := networkservice.NewNetworkServiceInterface(client)
 	var auth networkservice.Auth
 	authstr := "xxxx,yyyyy,NICE"
-	authtoken, err := service.GetAuthToken(&authstr)
-	if err != nil {
+
+	//authtoken, err := service.GetAuthToken(&authstr)
+	if err := client.Call("GetAuthToken", &authstr, &auth.Token); err != nil {
 		log.Fatalf("could't get auth token: %v", err)
 	}
-	auth.Token = *authtoken
+	//auth.Token = *authtoken
 	reply, err := service.GetDeviceInfo(&auth)
 	if err != nil {
 		log.Fatalf("could't get device info: %v", err)
