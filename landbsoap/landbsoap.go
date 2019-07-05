911f7fe3688dbea4aa6e3c527bbf36bd9b8a4a7a
Ignacio Reguero <Ignacio.Reguero@cern.ch>
2019-07-05 17:02:32 +0200

    first go at soap call

---
+++ landbsoap/landbsoap.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"crypto/tls"
+	"log"
+	"time"
+
+	"github.com/hooklift/gowsdl/soap"
+	"github.com/reguero/goauthor/landbsoap/networkservice"
+)
+
+func main() {
+	client := soap.NewClient(
+		"https://cstest.cern.ch/sc/soap/soap.fcgi?v=6&WSDL",
+		//"https://network.cern.ch/sc/soap/soap.fcgi?v=5&WSDL",
+		soap.WithTimeout(time.Second*5),
+		soap.WithTLS(&tls.Config{InsecureSkipVerify: true}),
+	)
+	service := networkservice.NewNetworkServiceInterface(client)
+	var auth networkservice.Auth
+	authstr := "xxxx,yyyyy,NICE"
+	authtoken, err := service.GetAuthToken(&authstr)
+	if err != nil {
+		log.Fatalf("could't get auth token: %v", err)
+	}
+	auth.Token = *authtoken
+	reply, err := service.GetDeviceInfo(&auth)
+	if err != nil {
+		log.Fatalf("could't get device info: %v", err)
+	}
+	log.Println(reply)
+}
